243227fa91b6a03a0a69a0dd8a3945534df69ea5
gnvera <va_gabriela@hotmail.com>
2019-07-24 01:44:47 -0500

    Admin roles permissions added

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Catálogo | SOMALI ')
+@section('title', 'Nuevo Producto | SOMALI ')
 
 @section('body-class', 'profile-page')
 
--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,16 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin/products', 'ProductController@index'); //Listado
-Route::get('/admin/products/create', 'ProductController@create'); //Crear nuevo
-Route::post('/admin/products', 'ProductController@store'); //registrar
-Route::get('/admin/products/{id}/edit', 'ProductController@edit'); //Formulario de edición
-Route::post('/admin/products/{id}/edit','ProductController@update'); //actualizar
-Route::delete('/admin/products/{id}', 'ProductController@destroy'); //Form eliminar
+Route::middleware(['auth','admin'])->prefix('admin')->group(function(){
+	Route::get('/products', 'ProductController@index'); //Listado
+	Route::get('/products/create', 'ProductController@create'); //Crear nuevo
+	Route::post('/products', 'ProductController@store'); //registrar
+	Route::get('/products/{id}/edit', 'ProductController@edit'); //Formulario de edición
+	Route::post('/products/{id}/edit','ProductController@update'); //actualizar
+	Route::delete('/products/{id}', 'ProductController@destroy'); //Form eliminar
+});
+
+
 
 //Put patch delete
 
